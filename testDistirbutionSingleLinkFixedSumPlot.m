14297f14b6d73994ba6cc61748b96c637e61db6c
Xu <yx4vf@ESERVICES.VIRGINIA.EDU>
2017-02-13 10:43:46 -0500

    Single link single hop, compare with TR as well

--- testDistirbutionSingleLinkFixedSumPlot.m
+++ testDistirbutionSingleLinkFixedSumPlot.m
@@ -61,27 +61,25 @@ for Nuser = 5:5:50
     seStdMatrix(:, k) = seStd;
     seMatrixAt10(:, k) = seGN(:, 6);
     legendStr = sprintf('%d users', Nuser);
-    h(k) = errorbar(axes1, distance, seAveMatrix(:, k), seStdMatrix(:, k), 'displayname', legendStr);
     if mod(k, 4)==1
         p(k) = histogram(axes2, seMatrixAt10(:, k), linspace(8.4, 8.52, 31), 'displayname', legendStr, 'normalization', 'probability');
+        h(k) = errorbar(axes1, distance, seAveMatrix(:, k), seStdMatrix(:, k), 'displayname', legendStr);
     end
     o(k) = plot(axes3, distance, seStdMatrix(:, k), 'displayname', legendStr);
     k = k+1;
 end
-tr = zeros(1000, 96);
-distance=5:100;
-for m=1:size(tr, 1)
-    for n=1:96
-        tr(m, n) = seSingleLink(dataRates(:, m), distance(n), systemParameters, 'TR');
-    end
-    fprintf('%d is done\n', m)
-end
+% tr = zeros(1000, 96);
+% for m=1:1000
+%     for n=1:96
+%         tr(m,n) = seSingleLink(dataRates(:, m), distance(n), systemParameters, 'TR');
+%     end
+% end
+load('data/testDistirbutionSingleLinkFixedSumPlot.mat', 'tr')
+plot(axes1, distance, mean(tr, 1), 'linewidth', 1, 'displayname', 'TR')
 
-h(end+1) = plot(axes1, distance, mean(tr, 1), 'displayname', 'TR');
 legend(axes1, 'show');
 xlabel(axes1, 'distance (100 km)')
 ylabel(axes1, 'ESE (bit/Hz)')
-
 legend(axes2, 'show');
 xlabel(axes2, 'ESE (bit/Hz)')
 ylabel(axes2, 'Probability')
