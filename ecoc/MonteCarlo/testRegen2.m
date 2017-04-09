85b0c31119137f8e408fc23ec5eda1ec807c86e3
LiYan1988 <li.yan.ly414@gmail.com>
2017-04-08 23:40:01 -0400

    Find shortest paths for BH

--- ecoc/MonteCarlo/testRegen2.m
+++ ecoc/MonteCarlo/testRegen2.m
@@ -98,17 +98,17 @@ SimulationParameters.Mbins = Mbins;
 SimulationParameters.Sbins = Sbins;
 
 %% Monte Carlo
-% tic
-% simulateNoiseRandomDemand(systemParameters, ...
-%     TopologyStruct, SimulationParameters, simuID);
-% runtimeMonteCarlo = toc;
+tic
+simulateNoiseRandomDemand(systemParameters, ...
+    TopologyStruct, SimulationParameters, simuID);
+runtimeMonteCarlo = toc;
 
 %% Sample noise
-tic
-SampleNoise = sampleNoiseRandomDemand(systemParameters, ...
-    TopologyStruct, SimulationParameters);
-runtimeSample = toc;
-save('SampleNoise.mat', 'SampleNoise');
+% tic
+% SampleNoise = sampleNoiseRandomDemand(systemParameters, ...
+%     TopologyStruct, SimulationParameters);
+% runtimeSample = toc;
+% save('SampleNoise.mat', 'SampleNoise');
 
 %% Regen for Monte Carlo
 % tic
@@ -118,11 +118,11 @@ save('SampleNoise.mat', 'SampleNoise');
 % runtimeRegenMC = toc;
 
 %% Regen for Sampling
-fprintf('Regen for sampling\n')
-tic
-regenStructSN = allocateRegenSN(systemParameters, TopologyStruct, ...
-    SimulationParameters, SampleNoise);
-runtimeRegenSN = toc;
+% fprintf('Regen for sampling\n')
+% tic
+% regenStructSN = allocateRegenSN(systemParameters, TopologyStruct, ...
+%     SimulationParameters, SampleNoise);
+% runtimeRegenSN = toc;
 
 %%
 % a = zeros(546, 14);
--- ecoc/MonteCarlo/updateP.m
+++ ecoc/MonteCarlo/updateP.m
@@ -1,10 +1,13 @@
-function P=updateP(P, D, cb)
+function [P, Cd]=updateP(P, D, cb, Cd)
 
 N = size(P, 1);
+n = 1;
 for i=1:N
     for j=1:N
         if (j~=cb) && (i~=cb) && (P(i, j)==0) && (P(i, cb)==1) && (P(cb, j)==1) && (D(i, cb)+D(cb, j)==D(i, j))
             P(i, j) = 1;
+            Cd(n, cb) = 1;
         end
+        n = n+1;
     end
 end
